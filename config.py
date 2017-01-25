67604beabd1daef40a665a3634991057fdf6a25d
wangzhiwei <wangzhiwei1996@outlook.com>
2017-01-26 01:24:14 +0800

    fix config

--- config.py
+++ config.py
@@ -1,12 +1,21 @@
 # -*- coding: utf8 -*-
 
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read('db.conf')
+dbhost = config.get('db', 'DBHOST')
+dbname = config.get('db', 'DBNAME')
+dbuser = config.get('db', 'DBUSER')
+passwd = config.get('db', 'PASSWORD')
+
 # 默认配置文件
 
 
 class Config(object):
     DEBUG = False
     SECRET_KEY = 'cDCDad5dFee25af54ED3Fd258A77cf2F'
-    SQLALCHEMY_DATABASE_URI = 'mysql://zhifeng:zhifengmysql@115.159.190.88/smarthome'
+    SQLALCHEMY_DATABASE_URI = 'mysql://{0}:{1}@{2}/{3}'.format(dbuser, passwd, dbhost, dbname)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     WEB_ADMIN = 'zhifeng523'
 
